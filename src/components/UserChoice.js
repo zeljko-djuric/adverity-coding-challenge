5914a8eff7964c7337a9e084871cbede282ee684
Zeljko Djuric <zeljko94@gmail.com>
2019-05-14 01:57:20 +0200

    Implemented suggestions for Select

--- src/components/UserChoice.js
+++ src/components/UserChoice.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import Typeahead from 'react-select';
+import Select from 'react-select';
 import axios from 'axios';
 
 class UserChoice extends React.Component{
 
-    constructor(){
-        super();
+    constructor(args){
+        super(args);
         this.state = {
             channel: [],
             campaign: [],
             clicks: [],
-            impressions: [],
+            impressions: []
         }
     }
 
+
     componentDidMount(){
         axios.get('http://www.mocky.io/v2/5cd93aeb300000b721c014b0').then(data => {
+
         // this.setState({info: data.data.split('\n')})
         //console.log('csv lines', data.data.split('\n'))
-        console.log(data.data.split('\n'))
+        // console.log('data:>>>>>>')
+        // console.log(data);
+        const rowData = data.data.split('\n');
+
+        rowData.map((item) => {
+            //console.log(item);
+            const result = item.split(',');
+            //console.log(result[0]);
+            
+            // this.setState({campaign: result[0]})
+            // this.setState({channel: result[1]})
+            // this.setState({clicks: result[2]})
+            // this.setState({impressions: result[3]})
+        })        
         this.setState({campaign: data.data.match(/[A-Z]{2} ?[|] ?[A-Z]{2} ?[|]? ?[A-Z][a-z]+ ?[|] ?[A-Z][a-z]+/g)})
         this.setState({channel: data.data.match(/Search|Display/g)})
         this.setState({clicks: data.data.match(/\b[0-9]+\b/g)})
@@ -27,14 +42,22 @@ class UserChoice extends React.Component{
     }
 
     render(){
+        var suggestions = [
+            {label: "Search"},
+            {label: "Display"}
+    ]
+        this.state.campaign.forEach(function(entry){
+            var singleObj = {}
+            singleObj['label'] = entry;
+            suggestions.push(singleObj);
+        });
         return(
             <div>
                 <h3>Choose channel or campaign:</h3>
-                <Typeahead/>
-                <h1>{this.state.campaign[0]}</h1>
-                <h1>{this.state.channel[0]}</h1>
-                <h1>{this.state.clicks[0]}</h1>
-                <h1>{this.state.impressions[0]}</h1>
+                <Select options={suggestions}/>
+                {/* <h1>{this.state.channel[1]}</h1>
+                <h1>{this.state.clicks[1]}</h1>
+                <h1>{this.state.impressions[1]}</h1> */}
             </div>
         )
     }
