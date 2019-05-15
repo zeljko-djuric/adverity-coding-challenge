737a5b99b5c8ff47d39e970d89db154e4e666681
Zeljko Djuric <zeljko94@gmail.com>
2019-05-16 01:18:13 +0200

    Header component defined as stateless

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import logo from '../img/logo.png';
 
-class Header extends React.Component{
-   
-    render(){
-        return(
-            <div className="header">
-                <img className="header_img" src={logo} alt="Logo"/>
-            </div>        
-    )
-    }
-}
+const Header = () =>(
+    <div className="header">
+        <img className="header_img" src={logo} alt="Logo"/>
+    </div>
+);
 
 export default Header;
