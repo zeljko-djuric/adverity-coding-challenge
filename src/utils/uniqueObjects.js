58f72040236add99af16a538e28c5c1ad74a653c
Zeljko Djuric <zeljko94@gmail.com>
2019-07-25 23:58:03 +0200

    Simplified uniqueObjects/removed DRY code

--- src/utils/uniqueObjects.js
+++ src/utils/uniqueObjects.js
@@ -1,10 +1,7 @@
 import _ from "lodash";
 
 const uniqueObjects = (campaign, channel) => {
-  const uniqueCampaign = _.uniqWith(campaign, _.isEqual);
-  const uniqeChannel = _.uniqWith(channel, _.isEqual);
-  const selectSuggestions = _.concat(uniqeChannel, uniqueCampaign);
-  return selectSuggestions;
+  return _.concat(_.uniqBy(campaign, "label"), _.uniqBy(channel, "label"));
 };
 
 export default uniqueObjects;
