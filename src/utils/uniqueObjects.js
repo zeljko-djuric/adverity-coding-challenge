9c48c4a2e684e8069e1118a0950ed72a162cdf26
Zeljko Djuric <zeljko94@gmail.com>
2019-07-26 09:23:42 +0200

    Removed return

--- src/utils/uniqueObjects.js
+++ src/utils/uniqueObjects.js
@@ -1,7 +1,6 @@
 import _ from "lodash";
 
-const uniqueObjects = (campaign, channel) => {
-  return _.concat(_.uniqBy(campaign, "label"), _.uniqBy(channel, "label"));
-};
+const uniqueObjects = (campaign, channel) =>
+  _.concat(_.uniqBy(campaign, "label"), _.uniqBy(channel, "label"));
 
 export default uniqueObjects;
