e3930bd24c19058065b931c37cb091594f1b1acb
Zeljko Djuric <zeljko94@gmail.com>
2019-08-23 09:37:24 +0200

    Improved extractOptions function

--- src/utils/extractOptions.js
+++ src/utils/extractOptions.js
@@ -1,16 +1,12 @@
 import _ from "lodash";
 
 const extractOptions = data => {
-  const options = [];
-  _.flatMap(data.data, item => {
-    const campaign = {};
-    const channel = {};
-    campaign["label"] = item.campaign;
-    channel["label"] = item.channel;
-    options.push(campaign);
-    options.push(channel);
-    return options;
-  });
+  const options = _.map(
+    _.flatMap(data.data, item => [item.campaign, item.channel]),
+    item => ({
+      label: item
+    })
+  );
   return _.uniqBy(options, "label");
 };
 export default extractOptions;
